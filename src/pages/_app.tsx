41b6426a071108da3980d511a5c632e99bf5f2ee
Hércules Câmara <39931772+HerculesCamara@users.noreply.github.com>
2024-01-30 23:42:57 -0300

    feat: creating header menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { globalStyles } from "@/styles/global"
 import { AppProps } from "next/app"
 
+import logoImg from "@/assets/logo.svg"
+import { Container, Header } from "@/styles/pages/app";
+
 globalStyles();
 
 function App({ Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
